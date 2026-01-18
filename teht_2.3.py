3d1d802d09eae332e6fdc5b15b4527ca9dbaefed
Vladislav Beresnev <128588731+Vlad-Beresnev@users.noreply.github.com>
2026-01-18 19:41:45 +0200

    tehtävä 2.3 on valmis

---
+++ teht_2.3.py
@@ -0,0 +1,6 @@
+kanta = float(input("Give a rectangle's basement: "))
+height = float(input("Give a rectangle's height: "))
+piiri = kanta * 2 + height * 2 
+pinta = kanta * height
+print(f"rectangle's perimeter: {piiri}")
+print(f"rectangle's area: {pinta}")
